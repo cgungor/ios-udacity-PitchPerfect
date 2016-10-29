50341a277bcab439e65da26c0694385f6d87d0b6
Cahit Gungor <cggungor@amazon.com>
2016-10-28 22:02:03 -0700

    Initial Commit

---
+++ PithcPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PithcPerfect
+//
+//  Created by Gungor, Cahit on 10/28/16.
+//  Copyright © 2016 Gungor, Cahit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
