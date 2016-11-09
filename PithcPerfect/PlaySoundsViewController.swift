5326b40df941958af04ef4451848d3fe499da4c3
Cahit Gungor <cggungor@amazon.com>
2016-11-08 21:16:50 -0800

    Audio Effects buttons added

--- PithcPerfect/PlaySoundsViewController.swift
+++ PithcPerfect/PlaySoundsViewController.swift
@@ -9,8 +9,26 @@
 import UIKit
 
 class PlaySoundsViewController: UIViewController {
+    
+    //  Outlets
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
     var recordedAudioURL : NSURL!
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopSoundForButton(sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
